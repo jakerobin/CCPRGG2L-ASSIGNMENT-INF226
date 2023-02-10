8834c92e17f3808bf3d08472c9f38478fefbbb92
jakerobin <122077529+jakerobin@users.noreply.github.com>
2023-02-10 14:55:56 +0800

    Create App.java

---
+++ exercise3/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+    Character Soldier = new Character();
+         Soldier.sayMyAttack();
+          weapon Sword = new weapon();
+        Sword.name = "devine";
+        Sword.damage = 80;
+        Sword.rarity = "legendary";
+        Sword.attackSpeed = 50;
+        Sword.sayMySword();
+        Sword.MyDamage(100);
+        Sword.NameRare();
+         System.out.println(Sword.NameRare());
+         }
+}
+
