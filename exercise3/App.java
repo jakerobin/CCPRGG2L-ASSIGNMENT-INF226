5c98b653cf5f4fa303d6ea94227b57ce25a03127
jakerobin <122077529+jakerobin@users.noreply.github.com>
2023-02-10 15:02:29 +0800

    Add files via upload

--- exercise3/App.java
+++ exercise3/App.java
@@ -1,16 +1,21 @@
 public class App {
-    public static void main(String[] args) {
-    Character Soldier = new Character();
-         Soldier.sayMyAttack();
-          weapon Sword = new weapon();
-        Sword.name = "devine";
-        Sword.damage = 80;
-        Sword.rarity = "legendary";
-        Sword.attackSpeed = 50;
-        Sword.sayMySword();
-        Sword.MyDamage(100);
-        Sword.NameRare();
-         System.out.println(Sword.NameRare());
-         }
-}
+    public static void main(String[] args) throws Exception {
+       
+        Person me = new Person();
 
+        // System.out.print(me.name);
+
+        // set method
+        me.setName("abcdef");
+        
+        // get method
+        System.out.println(me.getName());
+
+        // son object
+        Son myself = new Son();
+
+        System.out.println(myself.surname);
+    
+        myself.showSurname();
+    }
+}
---
+++ exercise3/exercise3.java
@@ -0,0 +1,17 @@
+public class Character {
+    int strenght;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName(){
+        System.out.println("Hello I am " + name);
+
+    }
+    public void sayMyStenght(){
+        System.out.println("My Strenght is " + strenght);   
+    }
+    public void sayMyAttack(){
+        System.out.println("I am attacking my enemies");
+    }
+}
